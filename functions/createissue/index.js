40b1270cf1996041a1e851135fa82c3296889377
alexiscolin <alexis@jaunebleu.co>
2024-10-05 02:21:23 +0900

    test15

--- functions/createissue/index.js
+++ functions/createissue/index.js
@@ -2,25 +2,29 @@ const { getInstallationToken } = require("./github-access");
 const { fetchYamlTemplate, convertYamlToMarkdown, getSupportedTypes } = require("./issue-templates");
 
 exports.handler = async function (event, context) {
-  const { type, data } = JSON.parse(event.body);
+  const { type, data } = JSON.parse(event.body); // Récupérer le type et les données associées
 
   try {
+    const { type, data } = JSON.parse(event.body);
+
     const supportedTypes = getSupportedTypes();
 
     if (!supportedTypes.includes(type)) {
       return {
         statusCode: 400,
-        body: JSON.stringify({ message: "Issue type not recognized" }),
+        body: JSON.stringify({ message: "Type d'issue non reconnu" }),
       };
     }
 
     const installationId = process.env.GITHUB_INSTALLATION_ID;
     const token = await getInstallationToken(installationId);
 
-    const yamlContent = fetchYamlTemplate(`${type.toLowerCase()}-add.yaml`);
+    const yamlTemplateFile = `${type.toLowerCase()}-add.yaml`;
+    const yamlContent = fetchYamlTemplate(yamlTemplateFile); // Récupérer le contenu YAML
+
     const issueBody = convertYamlToMarkdown(yamlContent, data);
 
-    const response = await fetch("https://github.com/alexiscolin/wallet-security-lists/issues", {
+    const response = await fetch(process.env.GITHUB_REPO_ISSUES_ENDPOINT, {
       method: "POST",
       headers: {
         Authorization: `token ${token}`,
@@ -28,11 +32,12 @@ exports.handler = async function (event, context) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        title: `[${type}-add]: ${data.name}`,
-        body: issueBody,
+        title: `[${type}-add]: ${data.name}`, // Titre basé sur le type et le nom soumis
+        body: issueBody, // Corps de l'issue en Markdown généré dynamiquement
       }),
     });
 
+    // Vérifier la réponse de l'API GitHub
     if (response.ok) {
       return {
         statusCode: 200,
